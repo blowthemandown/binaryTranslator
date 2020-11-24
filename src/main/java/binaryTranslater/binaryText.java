d034d430fb60baf68f931e1d8390b3cc82be8733
james <james@DESKTOP-G97TUT7>
2020-11-24 04:49:51 -0500

    began the methods

--- src/main/java/binaryTranslater/binaryText.java
+++ src/main/java/binaryTranslater/binaryText.java
@@ -9,6 +9,25 @@ package binaryTranslater;
  *
  * @author james
  */
-public class binaryText {
+public class binaryText implements textParser {
+    //String text;  well, maybe
     
+    //constructor
+    
+    public boolean isValid(String text){//should also check if the string is broken up into 8-bit 'words' or if it's all one chunk - by checking for spaces at position 8, 16 etc
+        for(int i = 0; i<text.length(); i++){
+            if(text.charAt(i)!='1'&&text.charAt(i)!='0')return false;
+            
+        }
+        return true;
+    }
+    
+    public String convertText(String text){
+        String s = "";
+        //put in a loop after breaking up the text string
+        int parseInt = Integer.parseInt(text, 2);
+        char c = (char)parseInt;
+        //s+=c
+        return s;
+    }
 }
