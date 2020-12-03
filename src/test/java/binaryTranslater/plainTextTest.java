eb0f76c14e6529b29004a09e2fc6ea53281e5240
james <james@DESKTOP-G97TUT7>
2020-12-03 16:58:46 -0500

    initiated test class

---
+++ src/test/java/binaryTranslater/plainTextTest.java
@@ -0,0 +1,34 @@
+/*
+ * Test cases for the plainText class
+ */
+package binaryTranslater;//needs to be the same as in the source package
+
+
+/**
+ *
+ * @author james
+ */
+
+import java.util.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+public class plainTextTest {
+    
+    plainText pt;
+    
+    
+    @BeforeClass
+    public static void SetUpClass() throws Exception{}
+    
+    @AfterClass
+    public static void TearDownClass() throws Exception{}
+    
+    
+    
+    
+    
+}
