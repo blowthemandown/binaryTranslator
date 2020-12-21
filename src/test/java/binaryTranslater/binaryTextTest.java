0f01fdf7278ea42cff16153d576648a67cc48ab5
james <james@DESKTOP-G97TUT7>
2020-12-21 14:04:59 -0500

    added framework for test class

--- src/test/java/binaryTranslater/binaryTextTest.java
+++ src/test/java/binaryTranslater/binaryTextTest.java
@@ -3,10 +3,31 @@
  */
 package binaryTranslater;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.After;
+
 /**
  *
  * @author james
  */
 public class binaryTextTest {
+    binaryText bt;
     
+    @BeforeClass
+    public static void SetUpClass() throws Exception{}
+    
+    @AfterClass
+    public static void TearDownClass() throws Exception{}
+    
+    @Before
+    public void setup(){
+        bt = new binaryText();
+    }
+    
+    @After
+    public void tearDown() throws Exception {}
 }
